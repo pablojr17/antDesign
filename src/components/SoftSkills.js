65e359ef8be684a7749a52156b9e8b0c19844bb0
pablojr17 <pablojr17@gmail.com>
2020-09-04 21:50:00 -0300

    Update

--- src/components/SoftSkills.js
+++ src/components/SoftSkills.js
@@ -13,11 +13,11 @@ const SoftSkills = () => {
         <h2>Habilidades comportamentais</h2>
           <li>Capacidade de engajar as pessoas</li>
           <li>Motivado</li>
-          <li>Comunicação</li>
-          <li>Empatia</li>
-          <li>Equilíbrio emocional</li>
-          <li>Resiliência</li>
-          <li>Bom humor</li>
+          <li>Comunicativo</li>
+          <li>Empatico</li>
+          <li>Equilíbrado</li>
+          <li>Resiliente</li>
+          <li>Bem humorado</li>
           <li>Autocrítico</li>
           <li>Determinado</li>
         </Content>
